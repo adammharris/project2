a354802258e993f69d4622738df78926c1c68bb7
adammharris <harrisadam42103@gmail.com>
2025-02-11 19:13:10 -0700

    Rightmost and leftmost points

--- convex_hull.py
+++ convex_hull.py
@@ -5,11 +5,12 @@
 
 def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the subset of provided points that define the convex hull"""
-    pivot = max(points, key=lambda p: (p[0], p[1]))
+
+    # Sorting algorithm: O(nlogn) time
+    pivot: tuple[float, float] = max(points, key=lambda p: (p[0], p[1]))
     points.sort(key=lambda p: math.atan2(p[1] - pivot[1], p[0] - pivot[0]), reverse=True)
+
     return divide(points)
-    # Sorting in place (using .sort()) reduces space complexity
-    # Time of sorting algorithm is O(nlogn)
 
 def divide(points: list[tuple(float, float)]) -> list[tuple[float, float]]:
     # Divide list into two parts in O(1) time
@@ -25,9 +26,38 @@ def divide(points: list[tuple(float, float)]) -> list[tuple[float, float]]:
     return conquer(left_half, right_half)
 
 def conquer(left_half: list[tuple[float, float]], right_half: list[tuple[float, float]]) -> list[tuple[float, float]]:
-    # Find rightmost point of left half, and leftmost point of right half in O(n) time
-    rightmost: tuple[float, float] = max(left_half, key=lambda p: (p[0], p[1]))
-    leftmost: tuple[float, float] = min(right_half, key=lambda p: (p[0], p[1]))
+    starting_points: tuple[tuple[float, float], tuple[float, float]] = tuple(random.choice(left_half), random.choice(right_half))
+    rightmost_position: int
+    leftmost_position: int
+    
+    # Find rightmost in left half in O(n) time
+    for key, point in enumerate(left_half):
+        if point[0] > starting_points[0][0][0]:
+            starting_points[0][0] = point
+            rightmost_position = key
+        elif point[0] == starting_points[0][0][0]:
+            if point[1] > starting_points[0][0][1]:
+                starting_points[0][0] = point
+                rightmost_position = key
+            else:
+                rightmost_position = key - 1
+    
+    # Find leftmost in right half O(n) time
+    for key, point in enumerate(right_half):
+        if point[0] > starting_points[0][0][0]:
+            starting_points[0][0] = point
+            rightmost_position = key
+        elif point[0] == starting_points[0][0][0]:
+            if point[1] > starting_points[0][0][1]:
+                starting_points[0][0] = point
+                rightmost_position = key
+            else:
+                rightmost_position = key - 1
+    
+    # Find upper tangent in O(n) time
+    upper_tangent = starting_points
+    # for key, point in enumerate(left_half):
+
 
     # Find upper tangent and lower tangent in O(n) time
     upper_tangent: tuple[tuple[float, float], tuple[float, float]] = tuple[rightmost, leftmost]
